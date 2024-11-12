ab2525be0d7bf90636ab263042fc725ff7c86b4b
hyorigo <67533928+hyorigo@users.noreply.github.com>
2024-11-12 09:11:29 +0800

    [feat] lib json: add try_ for all methods (#121)
    * works now
    
    * for shorter none
    
    * add missing
    
    * for unit tests
    
    * add for missing c
    
    * add for missing c2
    
    * add for missing c3
    
    * newer go versions for ci

--- lib/json/json.go
+++ lib/json/json.go
@@ -2,6 +2,8 @@
 package json
 
 import (
+	"bytes"
+	"encoding/json"
 	"sync"
 
 	itn "github.com/1set/starlet/dataconv"
@@ -15,6 +17,7 @@ import (
 const ModuleName = "json"
 
 var (
+	none       = starlark.None
 	once       sync.Once
 	jsonModule starlark.StringDict
 )
@@ -25,7 +28,11 @@ func LoadModule() (starlark.StringDict, error) {
 		mod := starlarkstruct.Module{
 			Name: ModuleName,
 			Members: starlark.StringDict{
-				"dumps": starlark.NewBuiltin("json.dumps", dumps),
+				"dumps":      starlark.NewBuiltin(ModuleName+".dumps", dumps),
+				"try_dumps":  starlark.NewBuiltin(ModuleName+".try_dumps", tryDumps),
+				"try_encode": starlark.NewBuiltin(ModuleName+".try_encode", tryEncode),
+				"try_decode": starlark.NewBuiltin(ModuleName+".try_decode", tryDecode),
+				"try_indent": starlark.NewBuiltin(ModuleName+".try_indent", tryIndent),
 			},
 		}
 		for k, v := range stdjson.Module.Members {
@@ -44,7 +51,7 @@ func dumps(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, k
 		indent = starlark.MakeInt(0)
 	)
 	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "obj", &obj, "indent?", &indent); err != nil {
-		return starlark.None, err
+		return none, err
 	}
 
 	// use 0 as default indent if failed to unpack indent
@@ -56,7 +63,71 @@ func dumps(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, k
 	// use internal marshaler to support starlark types
 	data, err := itn.MarshalStarlarkJSON(obj, int(it))
 	if err != nil {
-		return starlark.None, err
+		return none, err
 	}
 	return starlark.String(data), nil
+}
+
+func tryDumps(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var (
+		obj    starlark.Value
+		indent = starlark.MakeInt(0)
+	)
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "obj", &obj, "indent?", &indent); err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+
+	it, ok := indent.Int64()
+	if !ok || it < 0 {
+		it = 0
+	}
+
+	data, err := itn.MarshalStarlarkJSON(obj, int(it))
+	if err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{starlark.String(data), none}, nil
+}
+
+func tryEncode(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var x starlark.Value
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, kwargs, 1, &x); err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+
+	encoded, err := itn.EncodeStarlarkJSON(x)
+	if err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{starlark.String(encoded), none}, nil
+}
+
+func tryDecode(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var s string
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "x", &s); err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+
+	decoded, err := itn.DecodeStarlarkJSON([]byte(s))
+	if err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{decoded, none}, nil
+}
+
+func tryIndent(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var (
+		str    string
+		prefix string
+		indent string
+	)
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "str", &str, "prefix?", &prefix, "indent?", &indent); err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+
+	buf := new(bytes.Buffer)
+	if err := json.Indent(buf, []byte(str), prefix, indent); err != nil {
+		return starlark.Tuple{none, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{starlark.String(buf.String()), none}, nil
 }
